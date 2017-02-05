06088eff2a02ac036379797fdf015a06e0410a75
JLBarbie <jeanlaurent.barbie@gmail.com>
2017-02-05 17:58:39 +0100

    Fix: Systray init

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 
 #include "App.hpp"
+#include "Systray.hpp"
 #include <iostream>
 #include <unistd.h>
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char **argv) {
+    Systray tray = Systray(argc, argv);
     App leapmouse;
 
     return 0;
